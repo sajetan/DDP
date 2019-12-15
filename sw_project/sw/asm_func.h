42022cec637769795d04f4f98db28a34135aa00b
sajetan <sajetan@users.noreply.github.com>
2019-12-15 21:53:36 +0100

    primary interface is working

---
+++ sw_project/sw/asm_func.h
@@ -0,0 +1,43 @@
+/*
+ * asm_func.h
+ *
+ *  Created on: May 13, 2016
+ *      Author: dbozilov
+ */
+
+#ifndef ASM_FUNC_H_
+#define ASM_FUNC_H_
+
+#include <stdint.h>
+
+// a will be in register R0, b in R1, c in R2
+// Result is stored in register r0
+uint32_t add_3(uint32_t a, uint32_t b, uint32_t c);
+
+//Adds all elements of array
+uint32_t add_10(uint32_t *a, uint32_t n);
+
+//Copies array a to array b
+uint32_t arr_copy(uint32_t *a, uint32_t *b, uint32_t n);
+
+// Function that calculates {t[i+1], t[i]} = a[0]*b[0] + m[0]*n[0]
+// i is in R0, pointer to t array in R1, a array in R2, b array in R3
+// pointer to m array is stored in [SP]
+// pointer to n array is stored in [SP, #4] (one position above m)
+void multiply(uint32_t i, uint32_t *t, uint32_t *a, uint32_t *b, uint32_t *m, uint32_t *n);
+
+uint32_t test1();
+
+uint32_t opt_basic_add(uint32_t a, uint32_t b,uint32_t carry);
+uint32_t opt_multiply( uint32_t a, uint32_t b, uint32_t i, uint32_t *t, uint32_t c);
+uint32_t basic_multiply( uint32_t a, uint32_t b);
+
+//multiplication sq loop
+uint32_t mult_sqloop(uint32_t size, uint32_t *a, uint32_t *b, uint32_t *t);
+
+//multiplication loop
+uint32_t mult_loop(uint32_t size, uint32_t *a, uint32_t *b, uint32_t *t);
+//reduction+carry propogation loop
+uint32_t red_loop(uint32_t size, uint32_t n_prime_zero, uint32_t *n, uint32_t *t);
+
+#endif /* ASM_FUNC_H_ */
---
+++ sw_project/sw/montgomery.c
@@ -0,0 +1,183 @@
+/*
+ * montgomery.c
+ *
+ */
+#if 1
+
+#include "montgomery.h"
+#include <string.h>
+#include "common.h"
+void add_fun(uint32_t *t, uint32_t i, uint32_t c){
+	uint64_t sum;
+	while(c!=0){
+		sum =(uint64_t)t[i]+(uint64_t)c;
+		c=(uint32_t)(sum>> 32);
+		t[i]=sum;
+		i++;
+	}
+}
+
+
+void cond_sub(uint32_t * a, uint32_t* b, uint32_t *res,  uint32_t size){
+
+	for(uint32_t i=size-1; i>=0; i--){
+		if(a[i]<b[i]){arr_copy(a, res, 32); return;}
+		else break;
+	}
+
+	uint32_t carry = 0;
+
+	for(uint8_t i=0;i<size;i++){
+		res[i]=(a[i]-b[i]+carry);
+		if ((a[i])>=b[i]) carry=0;
+		else {
+			res[i]=4294967296+res[i] ;
+			carry=-1;
+		}
+	}
+
+}
+
+
+// Calculates res = a * b * r^(-1) mod n.
+// a, b, n, n_prime represent operands of size elements
+// res has (size+1) elements
+void montMul(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n_prime, uint32_t *res, uint32_t size)
+{
+
+	uint32_t t[65]={0x0};
+	uint32_t s;
+	uint32_t z [65]={0x0};
+	uint32_t temp_res [32]={0x0};
+
+	for(uint32_t i=0;i<size;i++){
+		uint32_t c=0;
+		for(uint32_t j=0;j<size;j++){
+			uint64_t sum =  (uint64_t) t[i+j]+(uint64_t)a[j]*(uint64_t)b[i]+(uint64_t)c;
+			c=(uint32_t)(sum>>32);
+			s=(uint32_t)sum;
+			t[i+j]=s;
+		}
+
+		t[i+size]=c;
+
+	}
+
+	for(uint32_t i=0;i<size;i++){
+		uint32_t c=0;
+		uint32_t z = (uint32_t)((uint64_t) t[i]*(uint64_t)n_prime[0]);
+		for (uint32_t j=0;j<size;j++){
+			uint64_t sum =  (uint64_t) t[i+j]+(uint64_t)z*(uint64_t)n[j]+(uint64_t)c;
+			c=sum>>32;
+			t[i+j]=(uint32_t)sum;
+		}
+		add_fun(t,i+size,c);
+	}
+
+
+	memcpy(temp_res, &t[size],2*8*size );
+	cond_sub(temp_res, n,res,size);
+
+}
+
+
+
+// Calculates res = a * b * r^(-1) mod n.
+// a, b, n, n_prime represent operands of size elements
+// res has (size+1) elements
+//Optimized montgomery multiplication
+void montMulOpt(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n_prime, uint32_t *res, uint32_t size)
+{
+
+	uint32_t t[65]={0x0};
+	uint32_t temp_res [33]={0x0};
+	uint32_t temp_n [33]={0x0};
+	mult_loop(size, a, b, t);
+
+	red_loop(size, n_prime[0], n, t);
+
+	arr_copy(&t[size], temp_res, 33);
+
+	if(temp_res[32]==1){
+		arr_copy(n, temp_n, 32);
+	    cond_sub(temp_res, temp_n, res,size+1);
+	}
+	else cond_sub(temp_res, n, res,size);
+
+
+}
+
+void montMulOptsq(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n_prime, uint32_t *res, uint32_t size)
+{
+
+	uint32_t t[65]={0x0};
+	uint32_t temp_res [33]={0x0};
+	uint32_t temp_n [33]={0x0};
+	mult_sqloop(size, a, b, t);
+
+	red_loop(size, n_prime[0], n, t);
+
+	arr_copy(&t[size], temp_res, 33);
+
+	if(temp_res[32]==1){
+		arr_copy(n, temp_n, 32);
+	    cond_sub(temp_res, temp_n, res,size+1);
+	}
+	else cond_sub(temp_res, n, res,size);
+
+
+}
+
+
+//uint32x4_t double_elements(uint32x4_t input)
+//{
+//return(vaddq_u32(input, input));
+//}
+
+
+uint32_t mont_exp(uint32_t *expres, uint32_t *message, uint32_t *n, uint32_t *n_prime, uint32_t *rmodn,uint32_t *r2modn,uint32_t *exp,uint32_t exp_len,uint32_t size){
+	uint32_t r=0;
+	uint32_t temp_A [32]={0x0};
+	uint32_t x_til [32]={0x0};
+	uint32_t One[32] = {1,0};
+	montMulOpt(message,r2modn,n,n_prime,x_til, size);
+	arr_copy(rmodn, temp_A, 32);
+	//xil_printf("exp_len = %d 0x%x\r\n", exp_len,exp[0]);
+	uint32_t i=exp_len;
+	//while(i>0){
+	for(i;i>0;i--){
+//		xil_printf("i val = %d %d\r\n", i-1, (exp[0]>>i-1)&1);
+//		xil_printf("A =");
+//		print_num(temp_A, 32);
+//		xil_printf("\n\r");
+
+		//START_TIMING
+		montMulOpt(temp_A,temp_A,n,n_prime,temp_A, size);
+		//STOP_TIMING
+		if ((exp[0]>>i-1)&1==1){
+//			xil_printf("after check A =");
+//			print_num(temp_A, 32);
+//			xil_printf("\n\r");
+			//START_TIMING
+			montMulOpt(temp_A,x_til,n,n_prime,temp_A, size);
+			//STOP_TIMING
+		}
+		//i--;
+
+	}
+
+
+
+	//START_TIMING
+	montMulOpt(temp_A,One,n,n_prime,expres, size);
+	//STOP_TIMING
+
+	return temp_A;
+
+}
+
+
+
+
+#endif
+
